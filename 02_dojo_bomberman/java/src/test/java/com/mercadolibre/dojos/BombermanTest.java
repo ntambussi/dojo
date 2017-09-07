7d447a67629c95bdfe8116be89c830d48b3737e1
diegosanchez <diego.sanchez@mercadolibre.com>
2017-09-07 13:10:41 -0300

    [bomberman]

---
+++ 02_dojo_bomberman/java/src/test/java/com/mercadolibre/dojos/BombermanTest.java
@@ -0,0 +1,71 @@
+package com.mercadolibre.dojos;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Tests for the dojo.
+ */
+public class BombermanTest {
+	
+	@Before
+	public void setup() {
+	}
+	
+	@Test
+	@Ignore
+	public void test_steps_into_a_cell_which_is_empty_then_it_moves_in() {
+		
+		// ... 
+        // Code here!
+        // ... 
+		
+	}
+
+	@Test
+	@Ignore
+	public void test_steps_into_a_cell_which_is_busy_with_an_object_then_it_does_not_move_and_throw_an_exception() {
+		
+		// ... 
+        // Code here!
+        // ... 
+		
+	}
+
+	@Test
+	@Ignore
+	public void test_steps_into_a_cell_which_is_busy_with_an_enemy_then_it_dies() {
+		
+		// ... 
+        // Code here!
+        // ... 
+		
+	}
+
+	@Test
+	@Ignore
+	public void test_leaves_a_boom_next_to_a_brick_the_boom_explotes_and_the_bricks_disappears() {
+		
+		// ... 
+        // Code here!
+        // ... 
+		
+	}
+
+	@Test
+	@Ignore
+	public void test_throws_a_boom_to_a_brick_the_boom_explotes_and_the_bricks_disappears() {
+		
+		// ... 
+        // Code here!
+        // ... 
+		
+	}
+	
+}
