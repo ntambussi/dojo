93e7e471e5c3e769663afc4625942683060d39c8
diegosanchez <diego.sanchez@mercadolibre.com>
2017-12-07 10:20:57 -0300

    [dojo_9] Travels excercises

---
+++ dojo_9/java/src/test/java/com/mercadolibre/dojos/DojoTest.java
@@ -0,0 +1,23 @@
+package com.mercadolibre.dojos;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Tests for the dojo.
+ */
+public class DojoTest {
+
+    @Test
+    public void test() throws FileNotFoundException {
+    }
+
+    @Test
+    public void onlyToAgree() throws FileNotFoundException {
+        Assert.assertEquals(1, 1);
+    }
+
+}
